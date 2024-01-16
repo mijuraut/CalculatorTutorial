86289b38df3264246113f045cd437b01345c23b8
Työ <Ty@MIKON>
2024-01-17 00:34:49 +0200

    Add project files.

---
+++ CalculatorTutorial.cpp
@@ -0,0 +1,36 @@
+// CalculatorTutorial.cpp : This file contains the 'main' function. Program execution begins and ends here.
+//
+
+#include <iostream>
+#include "Calculator.h"
+
+int main()
+{
+	double x = 0.0;
+	double y = 0.0;
+	double result = 0.0;
+	char oper = '+';
+
+	std::cout << "Calculator Console Application" << std::endl << std::endl;
+	std::cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b"
+		<< std::endl;
+	
+	Calculator c;
+	while (true)
+	{
+		std::cin >> x >> oper >> y;
+		if (oper == '/' && y == 0)
+		{
+			std::cout << " Attempted to divide by zero!" << std::endl;
+			continue;
+		}
+		else
+		{
+			result = c.Calculate(x, oper, y);
+		}
+		std::cout << "Result " << "of " << x << oper << y << " is: " << result << std::endl;
+	}
+	
+	return 0;
+}
+
